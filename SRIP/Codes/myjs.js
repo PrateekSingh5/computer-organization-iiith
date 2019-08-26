7dbb9e10328c294dd8ce38d9aec05fa35bc04d85
PrateekSingh5 <singhprateek692@gmail.com>
2019-08-26 16:18:05 +0530

    Sample Codes Updated

--- SRIP/Codes/myjs.js
+++ SRIP/Codes/myjs.js
@@ -788,3 +788,17 @@ function checkForSyntaxError(i) {
 		return !isLabel(i) && i.indexOf('.text')==-1;
 	}
 }
+
+function copyFunction(containerid) {
+if (document.selection) { 
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy"); 
+
+} else if (window.getSelection) {
+    var range = document.createRange();
+     range.selectNode(document.getElementById(containerid));
+     window.getSelection().addRange(range);
+     document.execCommand("copy");
+}}
